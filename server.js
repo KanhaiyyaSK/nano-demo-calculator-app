2288dfc98bee241c986043a5ddc9b17b2eb89b59
KanhaiyyaSK <97464146+KanhaiyyaSK@users.noreply.github.com>
2023-08-12 12:47:30 +0530

    don't loose

--- server.js
+++ server.js
@@ -1,28 +1,44 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
 const PORT = process.env.PORT || 8080;
 
-const baseUrl = '/calculator'
+const baseUrl = "/calculator";
 
 app.use(express.json());
 
 const baseRouter = express.Router();
 
-baseRouter.get('/greeting', (req, res) => {
-    return res.send('');
+baseRouter.get("/greeting", (req, res) => {
+  return res.send("Hello world");
 });
 
-baseRouter.post('/add', (req, res) => {
-    res.json({ "": null });
+baseRouter.post("/add", (req, res) => {
+  res.json({ "": null });
+
+  const { first, second } = req.body;
+
+  if (typeof first !== "number" || typeof second != "number") {
+    return res.status(400).json({ error: "Both inputs must be numbers" });
+  }
+  const result = first + second;
+
+  res.json({ result });
 });
 
 
-baseRouter.post('/subtract', (req, res) => {
-    res.json({ "": null });
+baseRouter.post("/subtract", (req, res) => {
+  res.json({ "": null });
+
+  if (typeof first !== "number" || typeof second != "number") {
+    return res.status(400).json({ error: "Both inputs must be numbers" });
+  }
+  const result = first - second;
+
+  res.json({ result });
 });
 
 app.use(baseUrl, baseRouter);
 app.listen(PORT, () => {
-    console.log("Server running at PORT", PORT);
+  console.log("Server running at PORT", PORT);
 });
